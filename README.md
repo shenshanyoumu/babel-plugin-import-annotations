87fcef3ebe05fb27d936a0c48fa8d26946956188
duduxiong <xiang_linzong@163.com>
2019-02-13 11:45:21 +0800

    docs: implement annotations for index.js

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
 # babel-plugin-import
 
-Modular import plugin for babel, compatible with [antd](https://github.com/ant-design/ant-design), [antd-mobile](https://github.com/ant-design/ant-design-mobile), lodash, [material-ui](http://material-ui.com/), and so on.
+在 Babel 配置中引入该插件，可以针对 [antd](https://github.com/ant-design/ant-design), [antd-mobile](https://github.com/ant-design/ant-design-mobile), lodash, [material-ui](http://material-ui.com/)等库进行按需加载.
 
 [![NPM version](https://img.shields.io/npm/v/babel-plugin-import.svg?style=flat)](https://npmjs.org/package/babel-plugin-import)
 [![Build Status](https://img.shields.io/travis/ant-design/babel-plugin-import.svg?style=flat)](https://travis-ci.org/ant-design/babel-plugin-import)
 
-----
+---
 
-## Why babel-plugin-import
+## 为什么需要 babel-plugin-import
 
 - [English Instruction](https://ant.design/docs/react/getting-started#Import-on-Demand)
 - [中文说明](https://ant.design/docs/react/getting-started-cn#%E6%8C%89%E9%9C%80%E5%8A%A0%E8%BD%BD)
 
-## Where to add babel-plugin-import
+## 如何在项目中添加 babel-plugin-import
 
 - [babelrc](https://babeljs.io/docs/usage/babelrc/)
 - [babel-loader](https://github.com/babel/babel-loader)
 
-## Example
+## 例子
 
 #### `{ "libraryName": "antd" }`
 
@@ -26,7 +26,7 @@ import { Button } from 'antd';
 ReactDOM.render(<Button>xxxx</Button>);
 
       ↓ ↓ ↓ ↓ ↓ ↓
-      
+
 var _button = require('antd/lib/button');
 ReactDOM.render(<_button>xxxx</_button>);
 ```
@@ -38,7 +38,7 @@ import { Button } from 'antd';
 ReactDOM.render(<Button>xxxx</Button>);
 
       ↓ ↓ ↓ ↓ ↓ ↓
-      
+
 var _button = require('antd/lib/button');
 require('antd/lib/button/style/css');
 ReactDOM.render(<_button>xxxx</_button>);
@@ -51,23 +51,21 @@ import { Button } from 'antd';
 ReactDOM.render(<Button>xxxx</Button>);
 
       ↓ ↓ ↓ ↓ ↓ ↓
-      
+
 var _button = require('antd/lib/button');
 require('antd/lib/button/style');
 ReactDOM.render(<_button>xxxx</_button>);
 ```
 
-Note : with `style: true` css source files are imported and optimizations can be done during compilation time. With `style: "css"`, pre bundled css files are imported as they are.  
+备注 : 配置 `style: true` 则在项目编译阶段，可以对引入的 antd 样式文件进行编译，从而可以压缩打包尺寸；而配置`style: "css"`, 则直接引入经过打包后的 antd 样式文件
 
-`style: true` can reduce the bundle size significantly, depending on your usage of the library.
-
-## Usage
+## 使用方式
 
 ```bash
 npm install babel-plugin-import --save-dev
 ```
 
-Via `.babelrc` or babel-loader.
+通过 `.babelrc` 配置文件或者 babel-loader 模块编程引入.
 
 ```js
 {
@@ -89,8 +87,8 @@ Via `.babelrc` or babel-loader.
 ```javascript
 {
   "libraryName": "lodash",
-  "libraryDirectory": "",
-  "camel2DashComponentName": false,  // default: true
+  "libraryDirectory": "", //表示从库的package.json的main入口；否则默认为lib文件夹
+  "camel2DashComponentName": false,  // default: true，将引入的组件名转化为"-"连接的文件名
 }
 ```
 
@@ -104,20 +102,21 @@ Via `.babelrc` or babel-loader.
 
 ~`options` can be an array.~ It's not available in babel@7+
 
-For Example: 
+For Example:
 
 ```javascript
 [
   {
-    "libraryName": "antd",
-    "libraryDirectory": "lib",   // default: lib
-    "style": true
+    libraryName: 'antd',
+    libraryDirectory: 'lib', // default: lib
+    style: true
   },
   {
-    "libraryName": "antd-mobile"
-  },
-]
+    libraryName: 'antd-mobile'
+  }
+];
 ```
+
 `Options` can't be an array in babel@7+, but you can add plugins with name to support multiple dependencies.
 
 For Example:
@@ -138,32 +137,34 @@ For Example:
 
 If option style is a `Function`, `babel-plugin-import` will auto import the file which filepath equal to the function return value. This is useful for the components library developers.
 
-e.g. 
-- ``["import", { "libraryName": "antd", "style": (name) => `${name}/style/2x` }]``: import js and css modularly & css file path is `ComponentName/style/2x`
+e.g.
+
+- `` ["import", { "libraryName": "antd", "style": (name) => `${name}/style/2x` }] ``: import js and css modularly & css file path is `ComponentName/style/2x`
 
 If a component has no style, you can use the `style` function to return a `false` and the style will be ignored.
 
-e.g. 
+e.g.
+
 ```js
 [
-  "import", 
-    { 
-      "libraryName": "antd", 
-      "style": (name: string, file: Object) => {
-        if(name === 'antd/lib/utils'){
-          return false;
-        }
-        return `${name}/style/2x`;
+  'import',
+  {
+    libraryName: 'antd',
+    style: (name: string, file: Object) => {
+      if (name === 'antd/lib/utils') {
+        return false;
       }
+      return `${name}/style/2x`;
     }
-]
+  }
+];
 ```
 
 #### customName
 
-We can use `customName` to customize import file path.
+使用 `customName` 来自定义导入文件路径.
 
-For example, the default behavior:
+插件默认导入文件的基础路径为 lib 目录，并且默认将引入的组件名转换为按照"-"连接的结构:
 
 ```typescript
 import { TimePicker } from "antd"
@@ -171,7 +172,7 @@ import { TimePicker } from "antd"
 var _button = require('antd/lib/time-picker');
 ```
 
-You can set `camel2DashComponentName` to `false` to disable transfer from camel to dash:
+设置 `camel2DashComponentName` 为 `false`来阻止组件名称的转换:
 
 ```typescript
 import { TimePicker } from "antd"
@@ -179,24 +180,24 @@ import { TimePicker } from "antd"
 var _button = require('antd/lib/TimePicker');
 ```
 
-And finally, you can use `customName` to customize each name parsing:
+在 Babel 配置文件中，使用 `customName` 来自定义导入组件路径:
 
 ```js
 [
-  "import", 
-    { 
-      "libraryName": "antd", 
-      "customName": (name: string) => {
-        if (name === 'TimePicker'){
-          return 'antd/lib/custom-time-picker';
-        }
-        return `antd/lib/${name}`;
+  'import',
+  {
+    libraryName: 'antd',
+    customName: (name: string) => {
+      if (name === 'TimePicker') {
+        return 'antd/lib/custom-time-picker';
       }
+      return `antd/lib/${name}`;
     }
-]
+  }
+];
 ```
 
-So this result is:
+上面编译后的结果为:
 
 ```typescript
 import { TimePicker } from "antd"
@@ -206,8 +207,8 @@ var _button = require('antd/lib/custom-time-picker');
 
 #### transformToDefaultImport
 
-Set this option to `false` if your module does not have a `default` export.
+如果打包后的模块没有`default`导出，则设置 `false`
 
-### Note
+### 备注
 
-babel-plugin-import will not work properly if you add the library to the webpack config [vendor](https://webpack.github.io/docs/code-splitting.html#split-app-and-vendor-code). 
+babel-plugin-import will not work properly if you add the library to the webpack config [vendor](https://webpack.github.io/docs/code-splitting.html#split-app-and-vendor-code).
--- src/Plugin.js
+++ src/Plugin.js
@@ -1,16 +1,26 @@
+// node的path模块，用于路径拼接
 import { join } from 'path';
-import { addSideEffect, addDefault, addNamed } from '@babel/helper-module-imports';
 
+// ES6 import时的辅助函数
+import {
+  addSideEffect,
+  addDefault,
+  addNamed
+} from '@babel/helper-module-imports';
+
+// 将类似TimerComponent字符串转换为timer-component形式，主要用于将组件引用转换为对应文件夹的引入
 function camel2Dash(_str) {
   const str = _str[0].toLowerCase() + _str.substr(1);
-  return str.replace(/([A-Z])/g, ($1) => `-${$1.toLowerCase()}`);
+  return str.replace(/([A-Z])/g, $1 => `-${$1.toLowerCase()}`);
 }
 
+// 将类似TimerComponent字符串转化为timer_component形式
 function camel2Underline(_str) {
   const str = _str[0].toLowerCase() + _str.substr(1);
-  return str.replace(/([A-Z])/g, ($1) => `_${$1.toLowerCase()}`);
+  return str.replace(/([A-Z])/g, $1 => `_${$1.toLowerCase()}`);
 }
 
+// Windows系统上，对文件路径分隔符的替换
 function winPath(path) {
   return path.replace(/\\/g, '/');
 }
@@ -29,49 +39,71 @@ export default class Plugin {
     index = 0
   ) {
     this.libraryName = libraryName;
-    this.libraryDirectory = typeof libraryDirectory === 'undefined'
-      ? 'lib'
-      : libraryDirectory;
-    this.camel2DashComponentName = typeof camel2DashComponentName === 'undefined'
-      ? true
-      : camel2DashComponentName;
+
+    // 默认为外部模块的lib文件夹进行按需加载转换
+    this.libraryDirectory =
+      typeof libraryDirectory === 'undefined' ? 'lib' : libraryDirectory;
+    this.camel2DashComponentName =
+      typeof camel2DashComponentName === 'undefined'
+        ? true
+        : camel2DashComponentName;
     this.camel2UnderlineComponentName = camel2UnderlineComponentName;
     this.style = style || false;
     this.fileName = fileName || '';
     this.customName = customName;
-    this.transformToDefaultImport = typeof transformToDefaultImport === 'undefined'
-      ? true
-      : transformToDefaultImport;
+    this.transformToDefaultImport =
+      typeof transformToDefaultImport === 'undefined'
+        ? true
+        : transformToDefaultImport;
+
+    // types表示Babel内置的AST节点类型
     this.types = types;
+
+    //针对多个babel-plugin-import插件实例对象设置
     this.pluginStateKey = `importPluginState${index}`;
   }
 
+  //
   getPluginState(state) {
     if (!state[this.pluginStateKey]) {
-      state[this.pluginStateKey] = {};  // eslint-disable-line
+      state[this.pluginStateKey] = {}; // eslint-disable-line
     }
     return state[this.pluginStateKey];
   }
 
   isInGlobalScope(path, name, pluginState) {
-    const parentPath = path.findParent((_path) =>
-    _path.scope.hasOwnBinding(pluginState.specified[name]));
+    const parentPath = path.findParent(_path =>
+      _path.scope.hasOwnBinding(pluginState.specified[name])
+    );
     return !!parentPath && parentPath.isProgram();
   }
 
+  /**
+   * import插件的辅助函数
+   * @param {*} methodName 处理该节点类型的函数名
+   * @param {*} file 导入的文件对象
+   * @param {*} pluginState 当前处理该节点类型的插件实例对象状态
+   */
   importMethod(methodName, file, pluginState) {
     if (!pluginState.selectedMethods[methodName]) {
       const libraryDirectory = this.libraryDirectory;
       const style = this.style;
-      const transformedMethodName = this.camel2UnderlineComponentName  // eslint-disable-line
+      const transformedMethodName = this.camel2UnderlineComponentName // eslint-disable-line
         ? camel2Underline(methodName)
         : this.camel2DashComponentName
-          ? camel2Dash(methodName)
-          : methodName;
+        ? camel2Dash(methodName)
+        : methodName;
       const path = winPath(
-        this.customName ? this.customName(transformedMethodName) : join(this.libraryName, libraryDirectory, transformedMethodName, this.fileName) // eslint-disable-line
+        this.customName
+          ? this.customName(transformedMethodName)
+          : join(
+              this.libraryName,
+              libraryDirectory,
+              transformedMethodName,
+              this.fileName
+            ) // eslint-disable-line
       );
-      pluginState.selectedMethods[methodName] = this.transformToDefaultImport  // eslint-disable-line
+      pluginState.selectedMethods[methodName] = this.transformToDefaultImport // eslint-disable-line
         ? addDefault(file.path, path, { nameHint: methodName })
         : addNamed(file.path, methodName, path);
       if (style === true) {
@@ -88,6 +120,13 @@ export default class Plugin {
     return Object.assign({}, pluginState.selectedMethods[methodName]);
   }
 
+  /**
+   * import插件的辅助函数
+   * @param {*} node
+   * @param {*} props
+   * @param {*} path
+   * @param {*} state
+   */
   buildExpressionHandler(node, props, path, state) {
     const file = (path && path.hub && path.hub.file) || (state && state.file);
     const types = this.types;
@@ -95,23 +134,37 @@ export default class Plugin {
     props.forEach(prop => {
       if (!types.isIdentifier(node[prop])) return;
       if (pluginState.specified[node[prop].name]) {
-        node[prop] = this.importMethod(pluginState.specified[node[prop].name], file, pluginState);  // eslint-disable-line
+        node[prop] = this.importMethod(
+          pluginState.specified[node[prop].name],
+          file,
+          pluginState
+        ); // eslint-disable-line
       }
     });
   }
 
+  /**
+   * import插件的辅助函数
+   * @param {*} node
+   * @param {*} prop
+   * @param {*} path
+   * @param {*} state
+   */
   buildDeclaratorHandler(node, prop, path, state) {
     const file = (path && path.hub && path.hub.file) || (state && state.file);
     const types = this.types;
     const pluginState = this.getPluginState(state);
     if (!types.isIdentifier(node[prop])) return;
-    if (pluginState.specified[node[prop].name] &&
+    if (
+      pluginState.specified[node[prop].name] &&
       path.scope.hasBinding(node[prop].name) &&
-      path.scope.getBinding(node[prop].name).path.type === 'ImportSpecifier') {
-      node[prop] = this.importMethod(node[prop].name, file, pluginState);  // eslint-disable-line
+      path.scope.getBinding(node[prop].name).path.type === 'ImportSpecifier'
+    ) {
+      node[prop] = this.importMethod(node[prop].name, file, pluginState); // eslint-disable-line
     }
   }
 
+  // 进入特定AST节点的钩子函数
   ProgramEnter(path, state) {
     const pluginState = this.getPluginState(state);
     pluginState.specified = Object.create(null);
@@ -120,10 +173,14 @@ export default class Plugin {
     pluginState.pathsToRemove = [];
   }
 
+  // 离开特定AST节点的钩子函数
   ProgramExit(path, state) {
-    this.getPluginState(state).pathsToRemove.forEach(p => !p.removed && p.remove());
+    this.getPluginState(state).pathsToRemove.forEach(
+      p => !p.removed && p.remove()
+    );
   }
 
+  // 处理AST中ImportDeclaration节点的函数，这就是import插件的能力所在。参数path和state由Babel编译器注入
   ImportDeclaration(path, state) {
     const { node } = path;
 
@@ -146,6 +203,7 @@ export default class Plugin {
     }
   }
 
+  // 处理AST中CallExpression节点的函数，这就是import插件的能力所在。参数path和state由Babel编译器注入
   CallExpression(path, state) {
     const { node } = path;
     const file = (path && path.hub && path.hub.file) || (state && state.file);
@@ -155,21 +213,32 @@ export default class Plugin {
 
     if (types.isIdentifier(node.callee)) {
       if (pluginState.specified[name]) {
-        node.callee = this.importMethod(pluginState.specified[name], file, pluginState);
+        node.callee = this.importMethod(
+          pluginState.specified[name],
+          file,
+          pluginState
+        );
       }
     }
 
     node.arguments = node.arguments.map(arg => {
       const { name: argName } = arg;
-      if (pluginState.specified[argName] &&
+      if (
+        pluginState.specified[argName] &&
         path.scope.hasBinding(argName) &&
-        path.scope.getBinding(argName).path.type === 'ImportSpecifier') {
-        return this.importMethod(pluginState.specified[argName], file, pluginState);
+        path.scope.getBinding(argName).path.type === 'ImportSpecifier'
+      ) {
+        return this.importMethod(
+          pluginState.specified[argName],
+          file,
+          pluginState
+        );
       }
       return arg;
     });
   }
 
+  // 处理AST中MemberExpression节点的函数，这就是import插件的能力所在。参数path和state由Babel编译器注入
   MemberExpression(path, state) {
     const { node } = path;
     const file = (path && path.hub && path.hub.file) || (state && state.file);
@@ -180,22 +249,31 @@ export default class Plugin {
 
     if (pluginState.libraryObjs[node.object.name]) {
       // antd.Button -> _Button
-      path.replaceWith(this.importMethod(node.property.name, file, pluginState));
+      path.replaceWith(
+        this.importMethod(node.property.name, file, pluginState)
+      );
     } else if (pluginState.specified[node.object.name]) {
-      node.object = this.importMethod(pluginState.specified[node.object.name], file, pluginState);
+      node.object = this.importMethod(
+        pluginState.specified[node.object.name],
+        file,
+        pluginState
+      );
     }
   }
 
+  // 处理AST中Property节点的函数，这就是import插件的能力所在。参数path和state由Babel编译器注入
   Property(path, state) {
     const { node } = path;
     this.buildDeclaratorHandler(node, 'value', path, state);
   }
 
+  // 处理AST中VariableDeclarator节点的函数，这就是import插件的能力所在。参数path和state由Babel编译器注入
   VariableDeclarator(path, state) {
     const { node } = path;
     this.buildDeclaratorHandler(node, 'init', path, state);
   }
 
+  // 处理AST中ArrayExpression节点的函数，这就是import插件的能力所在。参数path和state由Babel编译器注入
   ArrayExpression(path, state) {
     const { node } = path;
     const props = node.elements.map((_, index) => index);
@@ -209,7 +287,12 @@ export default class Plugin {
 
   ConditionalExpression(path, state) {
     const { node } = path;
-    this.buildExpressionHandler(node, ['test', 'consequent', 'alternate'], path, state);
+    this.buildExpressionHandler(
+      node,
+      ['test', 'consequent', 'alternate'],
+      path,
+      state
+    );
   }
 
   IfStatement(path, state) {
@@ -232,9 +315,12 @@ export default class Plugin {
     const { node } = path;
     const pluginState = this.getPluginState(state);
 
-    if (node.argument && types.isIdentifier(node.argument) &&
-    pluginState.specified[node.argument.name] &&
-    this.isInGlobalScope(path, node.argument.name, pluginState)) {
+    if (
+      node.argument &&
+      types.isIdentifier(node.argument) &&
+      pluginState.specified[node.argument.name] &&
+      this.isInGlobalScope(path, node.argument.name, pluginState)
+    ) {
       node.argument = this.importMethod(node.argument.name, file, pluginState);
     }
   }
@@ -249,6 +335,11 @@ export default class Plugin {
     this.buildExpressionHandler(node, ['left', 'right'], path, state);
   }
 
+  /**
+   *
+   * @param {*} path 表示待导入的模块路径对象
+   * @param {*} state 处理当前节点的插件实例状态
+   */
   NewExpression(path, state) {
     const { node } = path;
     this.buildExpressionHandler(node, ['callee', 'arguments'], path, state);
